5bd781963c610dca6b3cb978571ae2daf0043a5a
Lucas Henrique Rosa <lucas.henrique.r@hotmail.com>
2025-04-25 14:54:59 -0300

    🎨

--- src/components/layout/LayoutWrapper.tsx
+++ src/components/layout/LayoutWrapper.tsx
@@ -17,22 +17,22 @@ export default function LayoutWrapper({
 
   if (isPublicPage) {
     return (
-      <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <div className="min-h-screen bg-gray-50 flex items-center justify-center">
         {children}
       </div>
     );
   }
 
   return (
-    <div className="flex h-screen overflow-hidden bg-transparent ">
+    <div className="flex h-screen overflow-hidden bg-white ">
       {/* Desktop Sidebar */}
-      <div className="hidden md:flex p-1">
+      <div className="hidden md:flex p-1 ">
         <NavMenu />
       </div>
 
       <div className="flex-1 flex flex-col">
         <Header />
-        <main className="flex-1 overflow-auto bg-gray-50 p-6">{children}</main>
+        <main className="flex-1 overflow-auto  p-6">{children}</main>
         {/* <Footer /> */}
       </div>
     </div>
